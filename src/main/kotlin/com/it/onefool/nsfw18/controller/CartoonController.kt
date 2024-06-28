bfa032595a9531f771cdc903bbedbd31b9c9947a
Onefoo_l <974362219@qq.com>
2024-06-28 17:36:10 +0800

    添加查询漫画id接口

--- src/main/kotlin/com/it/onefool/nsfw18/controller/CartoonController.kt
+++ src/main/kotlin/com/it/onefool/nsfw18/controller/CartoonController.kt
@@ -1,5 +1,6 @@
 package com.it.onefool.nsfw18.controller
 
+import com.it.onefool.nsfw18.common.Result
 import com.it.onefool.nsfw18.service.CartoonService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 /**
  * @Author linjiawei
  * @Date 2024/6/26 3:52
+ * 漫画控制层
  */
 @RestController
 @RequestMapping("/cartoon")
@@ -18,6 +20,9 @@ class CartoonController {
     @Autowired
     private lateinit var cartoonService : CartoonService
 
+    /**
+     * 查看漫画id
+     */
     @GetMapping("/findId/{id}")
     fun findId(@PathVariable id : Int?):Result<Any>{
         return cartoonService.findId(id)
